2d75349b332f1e10a7f0b2802c11ad6400db8c05
Neil Deo <neildeo42@gmail.com>
2026-09-17 07:13:45 +0100

    Import terraform_ci SA

--- bootstrap/imports.tf
+++ bootstrap/imports.tf
@@ -2,3 +2,8 @@ import {
   to = google_storage_bucket.terraform_state
   id = "railway-analytics-508615-terraform-state"
 }
+
+import {
+  to = google_service_account.terraform_ci
+  id = "projects/railway-analytics-508615/serviceAccounts/terraform-ci@railway-analytics-508615.iam.gserviceaccount.com"
+}
