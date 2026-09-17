d84648d27e6c4eae18047d00c650cc84648517ed
Neil Deo <neildeo42@gmail.com>
2026-09-17 07:02:20 +0100

    Import tfstate storage bucket

---
+++ bootstrap/imports.tf
@@ -0,0 +1,4 @@
+import {
+  to = google_storage_bucket.terraform_state
+  id = "railway-analytics-508615-terraform-state"
+}
---
+++ bootstrap/storage.tf
@@ -0,0 +1,51 @@
+resource "google_storage_bucket" "terraform_state" {
+  name     = "railway-analytics-508615-terraform-state"
+  location = "EU"
+
+  uniform_bucket_level_access = true
+  public_access_prevention    = "enforced"
+
+  versioning {
+    enabled = true
+  }
+
+  soft_delete_policy {
+    retention_duration_seconds = 604800
+  }
+
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+
+    condition {
+      num_newer_versions = 20
+      with_state         = "ARCHIVED"
+    }
+  }
+
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+
+    condition {
+      days_since_noncurrent_time = 30
+      with_state                 = "ANY"
+    }
+  }
+
+  encryption {
+    google_managed_encryption_enforcement_config {
+      restriction_mode = "NotRestricted"
+    }
+
+    customer_managed_encryption_enforcement_config {
+      restriction_mode = "NotRestricted"
+    }
+
+    customer_supplied_encryption_enforcement_config {
+      restriction_mode = "FullyRestricted"
+    }
+  }
+}
