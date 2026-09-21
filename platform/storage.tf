74f17bfe6c9210c04bbc2eb9a907895bfb5f3426
Neil Deo <neildeo42@gmail.com>
2026-09-21 07:32:45 +0100

    Add storage bucket resource for landing raw data

---
+++ platform/storage.tf
@@ -0,0 +1,17 @@
+resource "google_storage_bucket" "network_rail_open_data_raw" {
+  name     = "${local.project_id}-network-rail-open-data-raw"
+  location = "EU"
+
+  storage_class = "STANDARD"
+
+  uniform_bucket_level_access = true
+  public_access_prevention    = "enforced"
+
+  versioning {
+    enabled = true
+  }
+
+  soft_delete_policy {
+    retention_duration_seconds = 604800
+  }
+}
