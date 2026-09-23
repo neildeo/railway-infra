3209594d237a7bf2aefc043cffe6f9f84c8d41be
Neil Deo <neildeo42@gmail.com>
2026-09-23 21:25:22 +0100

    Enable terraform-ci to create Cloud Run jobs in other roots

--- bootstrap/artifact_registry.tf
+++ bootstrap/artifact_registry.tf
@@ -1,10 +1,3 @@
-resource "google_project_service" "artifact_registry" {
-  project = local.project_id
-  service = "artifactregistry.googleapis.com"
-
-  disable_on_destroy = false
-}
-
 resource "google_artifact_registry_repository" "railway_containers" {
   project       = local.project_id
   location      = "europe-west1"
@@ -15,4 +8,13 @@ resource "google_artifact_registry_repository" "railway_containers" {
   depends_on = [
     google_project_service.artifact_registry,
   ]
+}
+
+resource "google_artifact_registry_repository_iam_member" "terraform_ci_reader" {
+  project    = local.project_id
+  location   = google_artifact_registry_repository.railway_containers.location
+  repository = google_artifact_registry_repository.railway_containers.repository_id
+
+  role   = "roles/artifactregistry.reader"
+  member = google_service_account.terraform_ci.member
 }
---
+++ bootstrap/services.tf
@@ -0,0 +1,13 @@
+resource "google_project_service" "artifact_registry" {
+  project = local.project_id
+  service = "artifactregistry.googleapis.com"
+
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "cloud_run" {
+  project = local.project_id
+  service = "run.googleapis.com"
+
+  disable_on_destroy = false
+}
