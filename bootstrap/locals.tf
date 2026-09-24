00b7cc7b63b114f3ea218368501c50d09794083c
Neil Deo <neildeo42@gmail.com>
2026-09-24 18:08:33 +0100

    Define CI deploy SA and wire into WIF

--- bootstrap/locals.tf
+++ bootstrap/locals.tf
@@ -1,7 +1,8 @@
 locals {
-  project_id           = "railway-analytics-508615"
-  github_owner_id      = "164882710"
-  github_repository_id = "1370194288"
+  project_id             = "railway-analytics-508615"
+  github_owner_id        = "164882710"
+  github_repository_id   = "1370194288"
+  railway_ingest_repo_id = "1384907199"
 
   terraform_artifacts_bucket_name = "${local.project_id}-terraform-artifacts"
 
